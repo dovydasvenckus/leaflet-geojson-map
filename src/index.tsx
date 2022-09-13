06804c3303251d2a67b33bf53445ed56ce0a142b
Dovydas Venckus <dovydas.venckus@live.com>
2022-09-13 21:01:16 +0300

    Make it more mobile friendly

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,12 @@ import { React } from "react";
 import ReactDOM from "react-dom/client";
 import GeoJsonMap from "./containers/GeoJsonMap";
 
-const centerCoordinates = [54.684, 25.275];
-const mapZoom = 13;
+const centerCoordinates = [54.684, 25.075];
+const mapZoom = 10;
 const tileLayerAttribution =
   '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 const tileLayerSource = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
-const root = ReactDOM.createRoot(document.getElementById("map"));
+const root = ReactDOM.createRoot(document.getElementById("map-wrapper"));
 const source = process.env.SOURCE_URLS.split(" ");
 
 root.render(
